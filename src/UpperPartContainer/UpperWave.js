ee2871e787553bef689cf8e7182afb07d75d8252
Ahmet Emre Parmaksız <aeparmaksiz20@gmail.com>
2022-11-15 21:13:39 +0300

    fixes6

--- src/UpperPartContainer/UpperWave.js
+++ src/UpperPartContainer/UpperWave.js
@@ -5,7 +5,7 @@ import "./UpperWave.css";
 function UpperWave() {
   return (
     <div>
-        <div className="wave wave1" style={{ backgroundImage: "url(./img/wave.png)" }} ></div>
+        <div className="wave wave1" style={{ backgroundImage: "url(./img/waveUpper.png)" }} ></div>
     </div>
   )
 }
