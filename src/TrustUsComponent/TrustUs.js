7c2d31bd982e37242a88065a2be9a60c9c00f537
Ahmet Emre Parmaksız <aeparmaksiz20@gmail.com>
2022-11-15 05:07:12 +0300

    fixes3

--- src/TrustUsComponent/TrustUs.js
+++ src/TrustUsComponent/TrustUs.js
@@ -3,24 +3,30 @@ import "./TrustUs.css";
 import iso from "./iso.svg";
 import gmp from "./gmp.png";
 
-
 function TrustUs() {
   return (
     <div className="trust-us-dom-container">
-        <div className='trust-us-title-container'><h1>Bize Güvenin</h1></div>
+      <div className="trust-us-title-container">
+        <h1>Bize Güvenin</h1>
+      </div>
       <div className="trust-us-container">
-        <div className="gmp-container">
-          <img src={gmp} alt="gmp"></img>
-        </div>
         <div className="trust-us-p-container">
-            <h2>Verilerinizin Gizliliği Bizim için Önemli</h2>
-            <p>GMP ve İSO belgelerimiz ve yüzlerce memnun müşterimizin de söylediği gibi Pelin Kozmetik içerik tariflerini gizli tutar, üretim ve teslimat sona erdikten sonra her şeyi ortadan kaldırır.</p></div>
-        <div className="iso-container">
-          <img src={iso} alt="iso"></img>
+          <h2>Verilerinizin Gizliliği Bizim için Önemli</h2>
+          <p>
+            GMP ve İSO belgelerimiz ve yüzlerce memnun müşterimizin de söylediği
+            gibi Pelin Kozmetik içerik tariflerini gizli tutar, üretim ve
+            teslimat sona erdikten sonra her şeyi ortadan kaldırır.
+          </p>
+        </div>
+        <div className="trust-us-image-container">
+          <div className="iso-container">
+            <img src={iso} alt="iso"></img>
+          </div>
+          <div className="gmp-container">
+            <img src={gmp} alt="gmp"></img>
+          </div>
         </div>
       </div>
-
-
     </div>
   );
 }
