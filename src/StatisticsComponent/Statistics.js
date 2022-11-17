8eb429bbd61804c5330e46908368711f4eb63412
Ahmet Emre Parmaksız <aeparmaksiz20@gmail.com>
2022-11-17 17:18:30 +0300

    responmsive

--- src/StatisticsComponent/Statistics.js
+++ src/StatisticsComponent/Statistics.js
@@ -14,6 +14,7 @@ function Statistics() {
         <div className='statistics-sub-title-container'><h2>Fason Üretimine Profesyonel Dokunuş</h2></div>
         <div className='statistics-sub-container'>
             <div className='statistics-upper'>
+                <div className='statistic-sub-container1'>
                 <div className='statistic-box'>
                     <div className='icon-container'><img src={statisticIcon1} alt="+250 Mutlu Müşteri"></img></div>
                     <div className='statistic'><p>250+</p><p>Mutlu Müşteri</p></div>
@@ -24,6 +25,8 @@ function Statistics() {
                     <div className='statistic'><p>150+</p><p>Proje</p></div>
                 </div>
 
+                </div>
+                <div className='statistic-sub-container2'>
                 <div className='statistic-box'>
                     <div className='icon-container'><img src={statisticIcon4} alt="+25 Profesyonel Çalışma"></img></div>
                     <div className='statistic'><p>25+</p><p>Profesyonel Çalışma</p></div>
@@ -33,6 +36,8 @@ function Statistics() {
                     <div className='icon-container'><img src={statisticIcon3} alt="30 Senelik Tecrübe"></img></div>
                     <div className='statistic'><p>30+</p><p>Senelik Tecrübe</p></div>
                 </div>
+                </div>
+                
             </div>
         </div>
     </div>
